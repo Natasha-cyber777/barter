b1b1c2199189d94b19165492066069b6605a3498
rnatasha093 <rnatasha093@gmail.com>
2026-02-12 14:21:59 +0530

    corrected sessions.py

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from app.db.base import Base
+# CRITICAL: You must import your models so Base 'registers' them
+from app.models.users import User 
 
 DATABASE_URL = "postgresql://postgres:rPNdAhQFnFZVHsZTmhQqPvmoWIzgOpox@postgres.railway.internal:5432/railway"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+
+# This command now 'sees' the User class and creates the "users" table
+Base.metadata.create_all(bind=engine)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
