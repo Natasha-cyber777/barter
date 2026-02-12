6a8edf308e8c5ae0f3ac85f8880193245bb70b93
rnatasha093 <rnatasha093@gmail.com>
2026-02-12 14:12:43 +0530

    Explicitly add bcrypt dependency

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://user:password@localhost/bartr"
+DATABASE_URL = "postgresql://postgres:rPNdAhQFnFZVHsZTmhQqPvmoWIzgOpox@postgres.railway.internal:5432/railway"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
